8c54d52a0174529837798429f041768fb7a2c8d1
joachimrasolofoarijaona-cmyk <joachim.rasolofoarijaona@acep.mg>
2025-12-03 09:10:47 +0300

    request recuperation added

--- resources/views/sim-requests/create-validator.blade.php
+++ resources/views/sim-requests/create-validator.blade.php
@@ -335,8 +335,6 @@ document.addEventListener('DOMContentLoaded', function() {
         if (type === 'recuperation') {
             const motif = document.getElementById('motif_recuperation');
             const motifHidden = document.getElementById('motif_hidden');
-
-            
             
             if (motif && motifHidden) {
                 motifHidden.value = motif.value;
