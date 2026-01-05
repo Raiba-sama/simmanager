9461432d13a0f5e554db87398d215d26af8e60e9
joachimrasolofoarijaona-cmyk <joachim.rasolofoarijaona@acep.mg>
2026-01-05 16:53:55 +0300

    update admin panel 16:53

--- resources/views/filament/custom-styles.blade.php
+++ resources/views/filament/custom-styles.blade.php
@@ -1,2 +1,30 @@
-<link rel="stylesheet" href="{{ asset('css/filament-dark-dropdowns.css') }}">
+<link rel="stylesheet" href="{{ asset('css/filament-dark-dropdowns.css') }}" />
+<style>
+/* Additional inline styles for dropdowns */
+.fi-select-option-list,
+.fi-select-option-list > div,
+.fi-dropdown-list,
+.fi-dropdown-list > div,
+[role="listbox"],
+[role="listbox"] > div {
+    background-color: #1f2937 !important;
+    color: #f9fafb !important;
+}
+
+.fi-select-option,
+.fi-dropdown-list-item,
+[role="option"] {
+    color: #f9fafb !important;
+}
+
+.fi-select-option:hover,
+.fi-select-option[data-highlighted],
+.fi-dropdown-list-item:hover,
+.fi-dropdown-list-item[data-highlighted],
+[role="option"]:hover,
+[role="option"][aria-selected="true"] {
+    background-color: #374151 !important;
+    color: #ffffff !important;
+}
+</style>
 
